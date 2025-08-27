d01676584fb6aec36a1a278a9f4a90a24493b809
Md. Moniruzzaman <mdmoniruzzamanbillal@gmail.com>
2025-08-27 12:58:44 +0600

    blog detail show

--- client/app/posts/[slug]/page.tsx
+++ client/app/posts/[slug]/page.tsx
@@ -1,17 +1,22 @@
+"use client";
 import Wrapper from "@/components/shared/Wrapper";
 import { Button } from "@/components/ui/button";
 import { Card, CardContent, CardHeader } from "@/components/ui/card";
+import { useGetSingleBlogs } from "@/hooks/blog.hooks";
+import { format } from "date-fns";
 import Link from "next/link";
+import { use } from "react";
 
 type PostDetailProps = {
-  params: {
-    slug: string;
-  };
+  params: Promise<{ slug: string }>;
 };
 
-const PostDetail = async ({ params }: PostDetailProps) => {
-  const slug = params?.slug;
-  console.log(slug);
+const PostDetail = ({ params }: PostDetailProps) => {
+  const { slug } = use(params);
+
+  const { data: blogData, isLoading } = useGetSingleBlogs(slug);
+
+  // console.log(blogData?.data);
 
   return (
     <div className="mainContainer min-h-screen py-3 bg-gray-100 ">
@@ -28,24 +33,27 @@ const PostDetail = async ({ params }: PostDetailProps) => {
           <Card>
             <CardHeader className="pb-6">
               <div className="flex items-center gap-4 text-sm text-muted-foreground mb-4">
-                <span>By post.author </span>
+                <span>By {blogData?.data?.author} </span>
                 <span>•</span>
-                <span> post.publishedAt </span>
+                <span>
+                  {blogData?.data?.createdAt &&
+                    format(
+                      new Date(blogData?.data?.createdAt as string),
+                      "MMMM dd,yyyy"
+                    )}
+                </span>
               </div>
               <h1 className="text-4xl font-bold text-foreground leading-tight">
-                {" "}
-                post.title{" "}
+                {blogData?.data?.title}
               </h1>
             </CardHeader>
             <CardContent>
-              <div className="prose prose-lg max-w-none">
-                <p>content </p>
-                <p>content </p>
-                <p>content </p>
-                <p>content </p>
-                <p>content </p>
-                <p>content </p>
-              </div>
+              <div
+                className=" blogContent  "
+                dangerouslySetInnerHTML={{
+                  __html: blogData?.data?.content,
+                }}
+              ></div>
             </CardContent>
           </Card>
         </div>
