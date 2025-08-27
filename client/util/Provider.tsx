6aabea7313c6efa677a6e101016aea363357e5c2
Md. Moniruzzaman <mdmoniruzzamanbillal@gmail.com>
2025-08-27 12:09:53 +0600

    react query integrated

---
+++ client/util/Provider.tsx
@@ -0,0 +1,21 @@
+"use client";
+
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { Toaster } from "sonner";
+
+import { ReactNode } from "react";
+
+interface ProvidersProps {
+  children: ReactNode;
+}
+
+const queryClient = new QueryClient();
+
+export default function Providers({ children }: ProvidersProps) {
+  return (
+    <QueryClientProvider client={queryClient}>
+      <Toaster />
+      {children}
+    </QueryClientProvider>
+  );
+}
